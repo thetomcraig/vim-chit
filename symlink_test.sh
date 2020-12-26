8e3728e7db8ad6ed2751b0f65cf8b6416770458c
Tom Craig <thetomcraig@icloud.com>
2020-12-25 22:58:02 -0800

    Add debug file

---
+++ symlink_test.sh
@@ -0,0 +1,4 @@
+rm ~/.vim/plugged/vim-chit/autoload/chit.vim
+rm ~/.vim/plugged/vim-chit/plugin/chit.vim
+ln -s /Users/tomcraig/.projects_root/vim-chit/autoload/chit.vim /Users/tomcraig/.vim/plugged/vim-chit/autoload/chit.vim
+ln -s /Users/tomcraig/.projects_root/vim-chit/plugin/chit.vim /Users/tomcraig/.vim/plugged/vim-chit/plugin/chit.vim
